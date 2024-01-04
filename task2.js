a10ca20d5baa0d82e7c56581518065b0a866f0fd
K0stya03 <96073763+K0stya03@users.noreply.github.com>
2024-01-04 23:04:45 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,8 +3,31 @@
 // Для перевірки завдання введіть команду npm jest task2.test.js
 
 
-// function applyStyles() {
- 
-// }
+function applyStyles() {
+    let titleElement = document.getElementById("title");
+    titleElement.style.fontWeight = "bold";
+    titleElement.style.backgroundColor = "green";
 
-// module.exports = applyStyles;
+    let myDiv = document.getElementById("myDiv").children;
+    myDiv[0].style.fontWeight = "bold";
+    myDiv[1].style.color = "red";
+    myDiv[2].style.textDecoration = "underline";
+    myDiv[3].style.fontStyle = "italic";
+
+    let myList = document.getElementById("myList");
+      myList.style.display = "flex";
+      myList.style.listStyleType = "none";
+      myList.style.padding = "0";
+
+    let spanElements = document.getElementsByTagName('span');
+    if (spanElements.length > 0) {
+        for (var i = 0; i < spanElements.length; i++) {
+          spanElements[i].style.display = 'none';
+        }
+    }
+ }
+ 
+ 
+ applyStyles();
+
+ module.exports = applyStyles;
